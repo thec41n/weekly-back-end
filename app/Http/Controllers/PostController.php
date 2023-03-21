3de8d0fa55bb0a361822d7f94517dbdeef934236
MikhaelEmpi <d.mikhaelempi@gmail.com>
2023-03-21 16:29:20 +0700

    feat: upload image function

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,9 +3,11 @@
 namespace App\Http\Controllers;
 
 use App\Models\Post;
+use Illuminate\Http\File;
 use Illuminate\Http\Request;
 use App\Http\Resources\PostResource;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Storage;
 use App\Http\Resources\PostDetailResource;
 
 class PostController extends Controller
@@ -30,6 +32,14 @@ class PostController extends Controller
             'blog_content' => 'required',
         ]);
         
+        if ($request->file) {
+            $fileName = $this->generateRandomString();
+            $extension = $request->file->extension();
+
+            Storage::putFileAs('image', $request->file, $fileName.'.'.$extension);
+        }
+        
+        $request['image'] = $fileName.'.'.$extension;
         // Jangan lupa Auth diimport
         $request['author'] = Auth::user()->id;
         $post = Post::create($request->all());
@@ -55,5 +65,15 @@ class PostController extends Controller
         $post->delete();
 
         return new PostDetailResource($post->loadMissing('writer:id,username'));
+    }
+
+    function generateRandomString($length = 30) {
+        $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+        $charactersLength = strlen($characters);
+        $randomString = '';
+        for ($i = 0; $i < $length; $i++) {
+            $randomString .= $characters[random_int(0, $charactersLength - 1)];
+        }
+        return $randomString;
     }
 }
